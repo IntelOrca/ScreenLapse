86e27268d5d8a6a7935b20c33fea1a071eea98b0
IntelOrca <ted@brambles.org>
2014-10-30 00:37:50 +0000

    initial commit

---
+++ IntelOrca.ScreenLapse/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace IntelOrca.ScreenLapse
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+		}
+	}
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# ScreenLapse
+A simple console application for capturing your screen by a certain interval
+for creation of time lapses.
